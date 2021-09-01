aa9803c033ec8682a49db51c20d254673fbd1752
allan.cao <allan.cao.liang@gmail.com>
2021-09-02 06:23:23 +0800

    upload models codes.

---
+++ run.py
@@ -0,0 +1,40 @@
+"""Flask CLI/Application entry point."""
+import os
+
+import click
+
+from metis_ai_services import create_app, db
+from metis_ai_services.models.token_blacklist import BlacklistedToken
+from metis_ai_services.models.user import User
+from metis_ai_services.models.widget import Widget
+
+app = create_app(os.getenv("FLASK_ENV", "development"))
+
+
+@app.shell_context_processor
+def shell():
+    return {
+        "db": db,
+        "User": User,
+        "BlacklistedToken": BlacklistedToken,
+        "Widget": Widget,
+    }
+
+
+@app.cli.command("add-user", short_help="Add a new user")
+@click.argument("email")
+@click.option("--admin", is_flag=True, default=False, help="New user has administrator role")
+@click.password_option(help="Do not set password on the command line!")
+def add_user(email, admin, password):
+    """Add a new user to the database with email address = EMAIL."""
+    if User.find_by_email(email):
+        error = f"Error: {email} is already registered"
+        click.secho(f"{error}\n", fg="red", bold=True)
+        return 1
+    new_user = User(email=email, password=password, admin=admin)
+    db.session.add(new_user)
+    db.session.commit()
+    user_type = "admin user" if admin else "user"
+    message = f"Successfully added new {user_type}:\n {new_user}"
+    click.secho(message, fg="blue", bold=True)
+    return 0
