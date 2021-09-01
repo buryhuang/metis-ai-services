2326e2c3e6d05f361fb48664b49e1e51de919078
allan.cao <allan.cao.liang@gmail.com>
2021-09-02 02:41:40 +0800

    add lint/formatter tools

---
+++ src/metis_ai_services/apis/__init__.py
@@ -0,0 +1,16 @@
+from flask_restx import Api
+
+from .dataframe import ns_dataframes, ns_dataframe
+from .datasets import ns_datasets, ns_dataset
+
+api = Api(
+    title="MetisAI API",
+    version="0.1.0",
+    description="MetisAI APIs",
+)
+
+api.add_namespace(ns_datasets, path="/datasets")
+api.add_namespace(ns_dataset, path="/dataset")
+
+api.add_namespace(ns_dataframes, path="/dataframes")
+api.add_namespace(ns_dataframe, path="/dataframe")
---
+++ src/metis_ai_services/apis/datasets.py
@@ -0,0 +1,71 @@
+from flask_restx import Namespace, Resource
+
+# endpoint: http(s)://[xxx-domain].metisai.com/datasets
+ns_datasets = Namespace(
+    "datasets", description="Custom or Trails datasets that provided by MetisAI."
+)
+
+
+# endpoint: http(s)://[xxx-domain].metisai.com/dataset
+ns_dataset = Namespace("dataset", description="Individual dataset RESTFul APIs")
+
+
+default_datasets = [
+    "cypto",
+    "covid",
+    "realestate",
+    "socialnetworkrelations",
+    "sports",
+    "socialissues",
+]
+
+
+@ns_datasets.route("/")
+class DataSetsRes(Resource):
+    """The Endpoint for the tables that user upload to MetiasAI Data Platform"""
+
+    def get(self):
+        """This gets list of all trails datasets
+
+        Returns:
+            list: All trails datasets in MetisAI.
+                  Example: ['cypto', 'covid', 'realestate', 'socialnetworkrelations', 'sports', 'socialissues', ...]
+        """
+        return default_datasets
+
+    def post(self):
+        """This creates a new dataset"""
+        pass
+
+
+@ns_dataset.route("/<string:dataset_id>")
+class DataSetRes(Resource):
+    """[summary]
+
+    Args:
+        Resource ([type]): [description]
+    """
+
+    def get(self, dataset_id):
+        """Get a DataSet
+
+        Args:
+            dataset_id ([type]): [description]
+        """
+        pass
+
+    def put(self, dataset_id):
+        """Update a DataSet
+
+        Args:
+            dataset_id ([type]): [description]
+        """
+        pass
+
+    def delete(self, dataset_id):
+        """Delete a DataSet
+
+        Args:
+            dataset_id ([type]): [description]
+        """
+        pass
